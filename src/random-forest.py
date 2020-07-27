0135e7b9b9e0f04b2937e9e5824bb94682e3c93c
Najib Ishaq <najib_ishaq@my.uri.edu>
2020-07-27 00:44:32 -0400

    starting random forest

--- src/random-forest.py
+++ src/random-forest.py
@@ -1,10 +1,11 @@
-from typing import List, Dict, Tuple
+import itertools
+from typing import List, Dict, Tuple, Union
 
 import numpy as np
-from pyclam import Manifold, criterion
+from pyclam import Manifold, criterion, Graph
 from sklearn.metrics import roc_auc_score
 
-from src import datasets
+from src import datasets as chaoda_datasets
 from src.methods import METHODS
 
 TRAIN_DATASETS = ['annthyroid', 'cardio', 'pima', 'shuttle', 'thyroid', 'vowels']
@@ -13,31 +14,61 @@ SUB_SAMPLE = 100_000
 MAX_DEPTH = 30
 
 
-def create_features():
-    space = (100, 100)
-    features = (np.indices(space).reshape(len(space), -1).T + 1)
-    features = np.asarray([row for row in features if row[0] >= row[1]])
+def lfd_features(graph: Graph) -> List[float]:
+    # TODO
+    pass
+
+
+def cardinality_features(graph: Graph) -> List[float]:
+    # TODO
+    pass
+
+
+def radii_features(graph: Graph) -> List[float]:
+    # TODO
+    pass
+
+
+def parent_child_features(graph: Graph) -> List[float]:
+    # TODO
+    pass
+
+
+def create_features(graph: Graph) -> List[float]:
+    feature_extractors = [
+        lfd_features,
+        cardinality_features,
+        radii_features,
+        parent_child_features,
+    ]
+    features: List[float] = list()
+    [features.extend(extractor(graph)) for extractor in feature_extractors]
     return features
 
 
-def score(upper: float, lower: float) -> Tuple[float, float]:
-    auc_scores: List[float] = list()
+def auc_scores(graph: Graph, labels: List[int]) -> List[float]:
+    # TODO: get auc scores for graph
+    pass
 
-    for dataset in TRAIN_DATASETS:
-        data, labels = datasets.read(dataset, normalize=NORMALIZE, subsample=SUB_SAMPLE)
-        manifold = Manifold(data, metric='euclidean').build(
-            criterion.MaxDepth(MAX_DEPTH),
-            criterion.LFDRange(upper, lower),
-        )
 
-        for method in METHODS:
-            anomalies: Dict[int, float] = METHODS[method](manifold)
-            y_true, y_score = list(anomalies.keys()), list(anomalies.values())
-            auc_scores.append(roc_auc_score(y_true, y_score))
+def create_training_data(filename: str, datasets: Union[List[str], None] = None):
+    if datasets is None:
+        datasets = TRAIN_DATASETS
 
-    return float(np.mean(auc_scores)), float(np.prod(auc_scores))
+    with open(filename, 'w') as fp:
+        for dataset in datasets:
+            data, labels = chaoda_datasets.read(dataset, normalize=NORMALIZE, subsample=SUB_SAMPLE)
+            manifold = Manifold(data, metric='euclidean').build(criterion.MaxDepth(MAX_DEPTH))
+            for layer in manifold.layers:
+                if layer.cardinality >= 32:
+                    features = create_features(layer)
+                    scores = auc_scores(layer, labels)
+                    features_line = ','.join([f'{f:.3f}' for f in features])
+                    scores_line = ','.join([f'{f:.3f}' for f in scores])
+                    fp.write(f'{features_line},{scores_line}\n')
+                    pass
+    return
 
 
 if __name__ == '__main__':
-    print(create_features())
-    print(create_features().shape)
+    pass
