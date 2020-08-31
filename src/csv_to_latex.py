9f3189a0c044604e7b4b9c4a37d47578024a2be3
Najib Ishaq <najib_ishaq@my.uri.edu>
2020-08-31 18:08:48 -0400

    broke out function to create individual tables

--- src/csv_to_latex.py
+++ src/csv_to_latex.py
@@ -1,4 +1,5 @@
 import os
+from pprint import pprint
 from typing import List, Dict
 
 from src.datasets import DATASETS
@@ -6,6 +7,7 @@ from src.utils import RESULTS_PATH
 
 TRAIN_DATASETS = ['annthyroid', 'mnist', 'pendigits', 'satellite', 'shuttle', 'thyroid']
 METRIC_NAMES = ['euclidean', 'manhattan']
+METHOD_NAMES = ['CC', 'PC', 'KN', 'SC']
 MEANS = ['amean', 'gmean', 'hmean']
 SELECTION_MODES = [
     'percentile',
@@ -24,7 +26,7 @@ def pyod_to_latex(filename: str):
 
 
 # noinspection DuplicatedCode
-def chaoda_to_latex(filename: str, ensemble_out: str, individual_out: str):
+def ensemble_to_latex(filename: str, ensemble_out: str, individual_out: str):
     ensemble_table: List[str] = list()
     individual_tables: Dict[str: List[str]] = {selection: list() for selection in SELECTION_MODES}
     with open(filename, 'r') as fp:
@@ -137,13 +139,74 @@ def chaoda_to_latex(filename: str, ensemble_out: str, individual_out: str):
     return
 
 
+def individual_to_latex(filename: str, individual_out: str):
+
+    individual_dict: Dict[str, Dict[str, Dict[str, Dict[str, Dict[str, float]]]]] = {
+        dataset: {
+            metric: {
+                selection: {
+                    mean: {
+                        method: -1 for method in METHOD_NAMES
+                    } for mean in MEANS
+                } for selection in SELECTION_MODES
+            } for metric in METRIC_NAMES
+        } for dataset in DATASETS.keys()
+    }
+
+    with open(filename, 'r') as fp:
+        _ = fp.readline().split(',')
+        for line in fp.readlines():
+            row = line.split(',')
+            [dataset, metric, selection] = row[:3]
+            values = row[-12:]
+
+            for i, mean in enumerate(MEANS):
+                for j, method in enumerate(METHOD_NAMES):
+                    index = j + i * len(MEANS)
+                    individual_dict[dataset][metric][selection][mean][method] = float(values[index])
+
+    column_names = ['dataset', 'metric'] + [name for _ in range(3) for name in METHOD_NAMES]
+    column_names = ['\\textbf{' + name + '}' for name in column_names]
+    headers = [
+        '\\begin{tabular}{|' + 'c|' * 14 + '}',
+        '\\hline',
+        ' &  & ' + ' & '.join(['\\multicolumn{4}{c|}{' + f'{mean}' + '}' for mean in MEANS]) + ' \\\\',
+        '\\hline',
+        ' & '.join(column_names) + ' \\\\',
+        '\\hline',
+    ]
+    individual_rows: Dict[str: List[str]] = {selection: list() for selection in SELECTION_MODES}
+    for dataset in DATASETS:
+        dataset_name = '\\textbf{' + dataset + '}' if dataset in TRAIN_DATASETS else dataset
+        for metric in METRIC_NAMES:
+            for selection in SELECTION_MODES:
+                individual_row: List[float] = [float(individual_dict[dataset][metric][selection][mean][method])
+                                               for method in METHOD_NAMES
+                                               for mean in MEANS]
+                individual_row: List[str] = [dataset_name, metric] + row_to_latex(individual_row)
+                individual_rows[selection].extend([' & '.join(individual_row) + ' \\\\', '\\hline'])
+    footer = '\\end{tabular}'
+
+    for selection in SELECTION_MODES:
+        outfile = individual_out[:-4] + f'_{selection}.txt'
+        with open(outfile, 'w') as fp:
+            [fp.write(f'{header}\n') for header in headers]
+            [fp.write(f'{row}\n') for row in individual_rows[selection]]
+            fp.write(footer)
+
+    return
+
+
 if __name__ == '__main__':
     os.makedirs(RESULTS_PATH, exist_ok=True)
 
     _chaoda_results = os.path.join(RESULTS_PATH, 'chaoda_predictions.csv')
-    _ensemble_out = os.path.join(RESULTS_PATH, 'ensemble_table.txt')
+
+    # _ensemble_out = os.path.join(RESULTS_PATH, 'ensemble_table.txt')
+    # ensemble_to_latex(_chaoda_results, _ensemble_out, _individual_out)
+
     _individual_out = os.path.join(RESULTS_PATH, 'individual_table.txt')
-    chaoda_to_latex(_chaoda_results, _ensemble_out, _individual_out)
+    individual_to_latex(_chaoda_results, _individual_out)
 
     # _pyod_comparisons = os.path.join(RESULTS_PATH, 'pyod_comparisons.csv')
     # pyod_to_latex(_pyod_comparisons)
