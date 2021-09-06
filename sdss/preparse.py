95f421e50572ba9c2adf65239e2e3939fe0341cb
Najib Ishaq <najib.ishaq@axleinfo.com>
2021-09-06 01:30:40 -0400

    more cleanup for ark

--- sdss/preparse.py
+++ sdss/preparse.py
@@ -9,9 +9,12 @@ from astropy.io import fits
 
 from utils import paths
 
-APOGEE_PATH = Path('/data/abd/sdss/stars/apo25m').resolve()
-APO25M_OUT_PATH = paths.DATA_DIR.joinpath('apo25m_dr16.npy')
-APO25M_METADATA_PATH = paths.DATA_DIR.joinpath('apo25m_dr16_filenames.csv')
+# APO_CHUNK = '1m'
+APO_CHUNK = '25m'
+
+APOGEE_PATH = Path(f'/data/abd/sdss/stars/apo{APO_CHUNK}').resolve()
+APOGEE_OUT_PATH = paths.DATA_DIR.joinpath(f'apo{APO_CHUNK}.npy')
+APOGEE_METADATA_PATH = paths.DATA_DIR.joinpath(f'apo{APO_CHUNK}_filenames.csv')
 NUM_DIMS = 8_575
 
 
@@ -22,38 +25,28 @@ def get_fits_file_paths() -> Dict[str, List[str]]:
     for field in tqdm(list(APOGEE_PATH.iterdir())):
         files = list(
             file_name.name for file_name in field.iterdir()
-            if 'apStar-r12-' in file_name.name
-            or 'asStar-r12-' in file_name.name
+            if 'apStar-' in file_name.name
+            or 'asStar-' in file_name.name
         )
         if len(files) > 0:
             fields_map[field.name] = files
 
-    # spectra_files = list()
-    # list(map(spectra_files.extend, fields_map.values()))
-    #
-    # field_ids = [
-    #     file_name.split('.')[0].split('-')[-1]
-    #     for file_name in spectra_files
-    # ]
-    #
-    # print(
-    #     f'num_fields: {len(fields_map)}, '
-    #     f'num_spectra: {len(spectra_files), len(set(spectra_files))}, '
-    #     f'num_ids: {len(field_ids), len(set(field_ids))}'
-    # )
+    num_spectra = sum(map(len, fields_map.values()))
+    print(f'collected {num_spectra} matching fits files for {APO_CHUNK}...')
+
     return fields_map
 
 
 def extract_combined_spectra(fields_map: Dict[str, List[str]], *, test_chunk: Optional[int] = None):
     num_spectra = sum(map(len, fields_map.values()))
-    apo25m_spectra = numpy.zeros(
+    apogee_spectra = numpy.zeros(
         shape=(num_spectra, NUM_DIMS),
         dtype=numpy.float32,
     )
     indices_to_remove = set()
 
     with tqdm(total=num_spectra) as progress_bar:
-        with open(APO25M_METADATA_PATH, 'w') as metadata_csv:
+        with open(APOGEE_METADATA_PATH, 'w') as metadata_csv:
             metadata_csv.write(f'field,fits_name\n')
 
         index = -1
@@ -77,10 +70,10 @@ def extract_combined_spectra(fields_map: Dict[str, List[str]], *, test_chunk: Op
                 else:
                     spectra = spectra.T
 
-                with open(APO25M_METADATA_PATH, 'a') as metadata_csv:
+                with open(APOGEE_METADATA_PATH, 'a') as metadata_csv:
                     metadata_csv.write(f'{field},{file}\n')
 
-                apo25m_spectra[index, :] = numpy.mean(spectra, axis=1)
+                apogee_spectra[index, :] = numpy.mean(spectra, axis=1)
 
                 progress_bar.update(1)
 
@@ -94,12 +87,12 @@ def extract_combined_spectra(fields_map: Dict[str, List[str]], *, test_chunk: Op
     if len(indices_to_remove) > 0:
         print(f'removing {len(indices_to_remove)} bad spectra...')
         indices_to_keep = list(filter(lambda i: i not in indices_to_remove, range(num_spectra)))
-        apo25m_spectra = apo25m_spectra[indices_to_keep, :]
+        apogee_spectra = apogee_spectra[indices_to_keep, :]
 
-    print(f'saving {apo25m_spectra.shape[0]} apo25m spectra...')
+    print(f'saving {apogee_spectra.shape[0]} apo{APO_CHUNK} spectra...')
     numpy.save(
-        file=APO25M_OUT_PATH,
-        arr=apo25m_spectra,
+        file=APOGEE_OUT_PATH,
+        arr=apogee_spectra,
         allow_pickle=False,
         fix_imports=False,
     )
@@ -109,4 +102,6 @@ def extract_combined_spectra(fields_map: Dict[str, List[str]], *, test_chunk: Op
 if __name__ == '__main__':
     paths.DATA_DIR.mkdir(exist_ok=True)
 
-    extract_combined_spectra(get_fits_file_paths(), test_chunk=10_000)
+    get_fits_file_paths()
+    # extract_combined_spectra(get_fits_file_paths())
+    # extract_combined_spectra(get_fits_file_paths(), test_chunk=10_000)
